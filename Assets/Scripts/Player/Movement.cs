648691872b51fa4506785c86c59cab8bc0c436a4
Unkreativboy <150541817+Unkreativboy@users.noreply.github.com>
2024-09-26 21:07:32 +0200

    thirdPersonController

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,11 +6,11 @@ public class Movement : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float movementSpeed;
-
-
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private Transform camera;
 
     private CharacterController characterController;
-     
+    private float turnSmoothVelocity;
 
 
     private void Start()
@@ -26,7 +26,21 @@ public class Movement : MonoBehaviour
     private void MovePlayer()
     {
         Vector2 moveInput =  GameInput.Instance.GetMovementVector();
-        characterController.SimpleMove(new Vector3(moveInput.x,0,moveInput.y)*movementSpeed);
+        Vector3 direction = new Vector3(moveInput.x, 0f, moveInput.y).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+        }
+
+
         
     }
 
